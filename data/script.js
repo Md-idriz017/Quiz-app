52a23ae8ccd0005163df029b6a99cf50297ca6ba
Md-idriz017 <idrizmohamed004@gmail.com>
2026-02-07 18:53:11 +0530

    Create data/script.js

---
+++ data/script.js
@@ -0,0 +1,163 @@
+export const userAnswer = [];
+
+export const questions = [
+  {
+    type: 'mcq',
+    question: 'What does DOM stand for?',
+    options: [
+      'Document Object Model',
+      'Data Object Model',
+      'Digital Order Model',
+      'Desktop Object Mode'
+    ],
+    correct: 0
+  },
+  {
+    type: 'tf',
+    question: 'JavaScript is a compiled language',
+    options: ['True', 'False'],
+    correct: 1
+  },
+  {
+    type: 'mcq',
+    question: 'Which keyword is used to declare a variable in JavaScript?',
+    options: ['var', 'int', 'string', 'define'],
+    correct: 0
+  },
+  {
+    type: 'tf',
+    question: 'JavaScript can run in the browser',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which method is used to select an element by ID?',
+    options: [
+      'querySelector()',
+      'getElementById()',
+      'getElementsByClassName()',
+      'querySelectorAll()'
+    ],
+    correct: 1
+  },
+  {
+    type: 'tf',
+    question: 'NaN means Not a Number',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which symbol is used for strict equality?',
+    options: ['==', '=', '===', '!='],
+    correct: 2
+  },
+  {
+    type: 'tf',
+    question: 'Arrays can store multiple data types',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which method adds an item to the end of an array?',
+    options: ['push()', 'pop()', 'shift()', 'unshift()'],
+    correct: 0
+  },
+  {
+    type: 'tf',
+    question: 'pop() removes the first element of an array',
+    options: ['True', 'False'],
+    correct: 1
+  },
+  {
+    type: 'mcq',
+    question: 'Which loop is best when number of iterations is known?',
+    options: ['for', 'while', 'do...while', 'foreach'],
+    correct: 0
+  },
+  {
+    type: 'tf',
+    question: 'Functions can return values',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which keyword stops a loop?',
+    options: ['stop', 'break', 'exit', 'return'],
+    correct: 1
+  },
+  {
+    type: 'tf',
+    question: 'const variables can be reassigned',
+    options: ['True', 'False'],
+    correct: 1
+  },
+  {
+    type: 'mcq',
+    question: 'Which data type is NOT primitive?',
+    options: ['String', 'Number', 'Object', 'Boolean'],
+    correct: 2
+  },
+  {
+    type: 'tf',
+    question: 'Objects store data in key-value pairs',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which method converts JSON to object?',
+    options: ['JSON.parse()', 'JSON.stringify()', 'JSON.convert()', 'JSON.object()'],
+    correct: 0
+  },
+  {
+    type: 'tf',
+    question: 'JSON.stringify() converts object to string',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which event runs when a button is clicked?',
+    options: ['submit', 'hover', 'click', 'change'],
+    correct: 2
+  },
+  {
+    type: 'tf',
+    question: 'addEventListener allows multiple handlers',
+    options: ['True', 'False'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which operator is used for logical AND?',
+    options: ['&&', '||', '!', '&'],
+    correct: 0
+  },
+  {
+    type: 'mcq',
+    question: 'Which keyword is used to create a function?',
+    options: ['func', 'function', 'def', 'method'],
+    correct: 1
+  },
+  {
+    type: 'tf',
+    question: 'setTimeout runs code repeatedly',
+    options: ['True', 'False'],
+    correct: 1
+  },
+  {
+    type: 'mcq',
+    question: 'Which method selects all matching elements?',
+    options: [
+      'getElementById()',
+      'querySelector()',
+      'querySelectorAll()',
+      'getElement()'
+    ],
+    correct: 2
+  }
+];
